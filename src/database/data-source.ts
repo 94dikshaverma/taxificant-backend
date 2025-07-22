8f7d4f4c3fa4d6ff4c37ccabaaf957dcb1b68c1c
Diksha Verma <94dikshaverma@gmail.com>
2025-07-22 22:26:43 +0530

    chore: update TypeORM configuration to use PostgreSQL instead of SQLite

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -9,8 +9,12 @@ dotenv.config();
 const configService = new ConfigService();
 
 export const AppDataSource = new DataSource({
-  type: 'mysql',
-  database: configService.get<string>('DATABASE_PATH', './database.sqlite'),
+  type: 'postgres',
+  host: configService.get<string>('DATABASE_HOST', 'localhost'),
+  port: configService.get<number>('DATABASE_PORT', 5432),
+  username: configService.get<string>('DATABASE_USERNAME', 'postgres'),
+  password: configService.get<string>('DATABASE_PASSWORD', ''),
+  database: configService.get<string>('DATABASE_NAME', 'taxificant_dev'),
   entities: [User],
   migrations: ['src/database/migrations/*.ts'],
   synchronize: false,
